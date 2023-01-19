f65b875f683ac793151052d54c12d7fd578595b5
Ismail Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-01-19 02:56:52 +0000

    another way to solve problem 21

--- index.js
+++ index.js
@@ -245,6 +245,9 @@ let number = function(busStops) {
   }
   return totalGet - totalOff;
 }
+//another way to solve problem 21
+// const number = (busStops) => busStops.reduce((rem, [on, off]) => rem + on - off, 0);
+
 // console.log(number([[10, 0], [3, 5], [5, 8]]))
 // console.log(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]))
 console.log(number([[0,0]]))

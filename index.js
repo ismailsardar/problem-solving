249351eeb23d817a20577a2e68b341d3ec33746d
Ismail Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-01-21 11:48:22 +0000

    problem 28 solving other way

--- index.js
+++ index.js
@@ -343,17 +343,20 @@ function otherAngle(a, b) {
 
 //problem = 28
 const quarterOf = (month) => {
-  if (month > 0 && month <= 3) {
-    return 1;
-  } else if (month >= 4 && month <= 6) {
-    return 2;
-  } else if (month >= 7 && month <= 9) {
-    return 3;
-  } else if (month >= 10 && month <= 12) {
-    return 4;
-  } else {
-    return 0;
-  }
+  
+  return Math.ceil(month/3)
+  
+  // if (month > 0 && month <= 3) {
+  //   return 1;
+  // } else if (month >= 4 && month <= 6) {
+  //   return 2;
+  // } else if (month >= 7 && month <= 9) {
+  //   return 3;
+  // } else if (month >= 10 && month <= 12) {
+  //   return 4;
+  // } else {
+  //   return 0;
+  // }
 
 }
 console.log(quarterOf(11))

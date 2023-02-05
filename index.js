83df36946c2805db1aa1156ce37099e9d899acc7
Ismail Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-02-05 08:26:02 +0000

    solved problem 40

--- index.js
+++ index.js
@@ -468,4 +468,123 @@ var hotpo = function(n,acc=0){
 }
 // if(number is even) number = number / 2
 // if(number is odd) number = 3*number + 1
-console.log(hotpo(6))
+// console.log(hotpo(6))
+
+//problem = 39
+function SubtractSum(n){
+  let sum = 0;
+  let numStr = n.toString().split('');
+  let numArr = numStr.map(num=> sum+=Number(num));
+  // let 
+  return n-sum;
+}
+// console.log(SubtractSum(12))
+let frute = [
+ "kiwi",
+"pear",
+"kiwi",
+"banana",
+"melon",
+"banana",
+"melon",
+"pineapple",
+"apple",
+"pineapple",
+"cucumber",
+"pineapple",
+"cucumber",]
+// "orange",
+// 15-grape
+// 16-orange
+// 17-grape
+// 18-apple
+// 19-grape
+// 20-cherry
+// 21-pear
+// 22-cherry
+// 23-pear
+// 24-kiwi
+// 25-banana
+// 26-kiwi
+// 27-apple
+// 28-melon
+// 29-banana
+// 30-melon
+// 31-pineapple
+// 32-melon
+// 33-pineapple
+// 34-cucumber
+// 35-orange
+// 36-apple
+// 37-orange
+// 38-grape
+// 39-orange
+// 40-grape
+// 41-cherry
+// 42-pear
+// 43-cherry
+// 44-pear
+// 45-apple
+// 46-pear
+// 47-kiwi
+// 48-banana
+// 49-kiwi
+// 50-banana
+// 51-melon
+// 52-pineapple
+// 53-melon
+// 54-apple
+// 55-cucumber
+// 56-pineapple
+// 57-cucumber
+// 58-orange
+// 59-cucumber
+// 60-orange
+// 61-grape
+// 62-cherry
+// 63-apple
+// 64-cherry
+// 65-pear
+// 66-cherry
+// 67-pear
+// 68-kiwi
+// 69-pear
+// 70-kiwi
+// 71-banana
+// 72-apple
+// 73-banana
+// 74-melon
+// 75-pineapple
+// 76-melon
+// 77-pineapple
+// 78-cucumber
+// 79-pineapple
+// 80-cucumber
+// 81-apple
+// 82-grape
+// 83-orange
+// 84-grape
+// 85-cherry
+// 86-grape
+// 87-cherry
+// 88-pear
+// 89-cherry
+// 90-apple
+// 91-kiwi
+// 92-banana
+// 93-kiwi
+// 94-banana
+// 95-melon
+// 96-banana
+// 97-melon
+// 98-pineapple
+// 99-apple
+// 100-pineapple
+// ]
+
+//problem = 40
+function solution(str){
+  const reversesStr = str.split('').reverse().join('');
+  return reversesStr;
+}
+console.log(solution("str"))

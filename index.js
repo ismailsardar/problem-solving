95ffec4e1dc49f4b4f43726becab6e866abf6e22
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-02-16 09:15:08 +0000

    Fun with ES6 Classes #1 - People, people, people / problem 51

--- index.js
+++ index.js
@@ -722,4 +722,23 @@ function squareSum(numbers){
   // }
   // return sum;
 }
-console.log(squareSum([1,2,2]))
+// console.log(squareSum([1,2,2]))
+
+//problem = 51
+// Fun with ES6 Classes #1 - People, people, people
+class Person {
+  constructor(firstName='John',lastName='Doe',age=0,gender='Male'){
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.age = age,
+    this.gender = gender
+  }
+
+  sayFullName(){
+    return this.firstName + ' ' + this.lastName;
+  }
+
+  static greetExtraTerrestrials(raceName){
+    return `Welcome to Planet Earth ${raceName}`
+  }
+}

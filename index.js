c0ee425d7c09a0d3c41a512d1aac1bbdb21d7998
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-02-16 09:18:52 +0000

    class constructor diclear another way

--- index.js
+++ index.js
@@ -726,12 +726,12 @@ function squareSum(numbers){
 
 //problem = 51
 // Fun with ES6 Classes #1 - People, people, people
-class Person {
-  constructor(firstName='John',lastName='Doe',age=0,gender='Male'){
-    this.firstName = firstName,
-    this.lastName = lastName,
-    this.age = age,
-    this.gender = gender
+class Person {  constructor(firstName='John',lastName='Doe',age=0,gender='Male'){
+    Object.assign(this, { firstName, lastName, age, gender });
+    // this.firstName = firstName,
+    // this.lastName = lastName,
+    // this.age = age,
+    // this.gender = gender
   }
 
   sayFullName(){

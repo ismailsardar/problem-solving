0816b6788cd94b20e866544032d6614198fd6c96
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-03-04 17:52:42 +0000

    Vowel one / problem = 74

--- problem1.js
+++ problem1.js
@@ -1,8 +1,21 @@
 //problem = 73
-let solution =(str, ending)=> str.endsWith(ending)
+let solution = (str, ending) => str.endsWith(ending)
 
-function solution1(str, ending){
+function solution1(str, ending) {
   console.log(-ending.length)
   return str.substr(-ending.length) == ending;
 }
-console.log(solution1('Hello','lo'))
+console.log(solution1('Hello', 'lo'));
+
+console.clear();
+//problem = 74
+
+function vowelOne(s){
+  let vowel = ['a','e','i','o','u'];
+  let str = ''
+  s.toLowerCase().split('').map((ele)=> vowel.includes(ele) ? str+=1 : str+=0);
+  return str;
+}
+
+console.log(vowelOne('vowelOne'))
+console.log(12)

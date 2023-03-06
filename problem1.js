7c9fd4d5ca8e7650f200eda2bb89163d26c2c92a
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-03-06 13:49:38 +0000

    problem 77 solving another way

--- problem1.js
+++ problem1.js
@@ -10,18 +10,18 @@ console.log(solution1('Hello', 'lo'));
 console.clear();
 //problem = 74
 
-function vowelOne(s){
-  let vowel = ['a','e','i','o','u'];
+function vowelOne(s) {
+  let vowel = ['a', 'e', 'i', 'o', 'u'];
   let str = ''
-  s.toLowerCase().split('').map((ele)=> vowel.includes(ele) ? str+=1 : str+=0);
+  s.toLowerCase().split('').map((ele) => vowel.includes(ele) ? str += 1 : str += 0);
   return str;
 }
 
-function vowelOne1(s){
+function vowelOne1(s) {
   return s.replace(/[^aeiou]/gi, '0').replace(/[^\d]/g, '1');
 }
 
-function vowelOne2(s){
+function vowelOne2(s) {
   return s.split('').map(x => ('aeiouAEIOU'.includes(x)) ? 1 : 0).join('');
 }
 console.log(vowelOne2('vowelOne'))
@@ -29,17 +29,17 @@ console.log(12)
 
 // console.clear();
 //problem = 75
-function none(arr, fun){
- return !arr.some(ele => fun(ele));
+function none(arr, fun) {
+  return !arr.some(ele => fun(ele));
 }
 
-function none1(arr, fun){
+function none1(arr, fun) {
   for (var i = 0; i < arr.length; i++) {
     if (fun(arr[i]) == true) {
       return false;
     }
   }
-  
+
   return true;
 }
 console.log(none())
@@ -48,7 +48,16 @@ console.clear();
 //problem = 77
 // Classy Extentions
 class Cat extends Animal {
-  speak(){
+  speak() {
     return this.name + ' meows.'
+  }
+}
+
+class Cat1 extends Animal {
+  constructor(value) {
+    super(value)
+  }
+  speak() {
+    return `${this.name} meows.`
   }
 }
